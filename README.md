c247896281e7c6e37023fbaefb50d105dacbd00c
Jimit Raval <34153272+jimmi1998@users.noreply.github.com>
2020-05-30 00:06:12 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Landing Page Project
+
+## Overview
+
+This project is a multi-sectional page which can be navigated dynamically. When clicking on any section from header it navigates to the specifc section. The header is fixed so that the sections can be easily clicked from any part of the page.
+
+Ii was build based on the starter project provided by Udacity, which has some HTML and CSS styling to display a static version of the Landing Page project. I convert the starter project from a static project to an interactive one. This requires modifying the HTML and CSS files, but primarily the JavaScript file.
+
+
+## Project Rubric
+Some key rubrics include
+    * All files shown must be present and the app must successfully render a home page with clear design and functionality added when index.html is loaded in the browser. No errors should display in console.
+    * All features are usable across modern desktop, tablet, and phone  browsers.
+    * Styling has been added for active states.
+    * There are at least 4 sections that have been added to the page.
+    * Navigation is built dynamically as an unordered list.
+    * It should be clear which section is being viewed while scrolling through the page.
+    * When clicking an item from the navigation menu, the link should scroll to the appropriate section.
---
+++ js/app.js
@@ -0,0 +1,169 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+// create sectionList object
+const sections= document.querySelectorAll("section");
+
+let sectionIDs = []
+let sectionDataNavs = [];
+for (const section of sections){
+    const sectionID = section.getAttribute("id")
+    const sectionDataNav = section.getAttribute("data-nav");
+    
+    sectionIDs.push(sectionID);
+    sectionDataNavs.push(sectionDataNav);
+}
+
+
+let sectionList = {};
+sectionIDs.forEach((key, i) => sectionList[key] = sectionDataNavs[i]);
+
+// get navbar__list id
+const navbarList = document.getElementById("navbar__list");
+
+// get header tag
+const headerTag = document.querySelector("header");
+
+// // get .navbar__link
+// const navbarLinksList = document.getElementsByClassName("navbar__link");
+// //const navbarLinksList = document.querySelectorAll(".navbar__link"); //this won't work because https://stackoverflow.com/questions/18735188/queryselectorall-not-working
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav + add href links 
+function createUL(sectionID){
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.textContent = sectionList[sectionID];
+    a.setAttribute("href", "#"+sectionID);
+    a.setAttribute("class", "navbar__link");
+    navbarList.appendChild(li).appendChild(a);
+}
+
+function createHeaderList(){
+    //for header
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.textContent = "Home";
+    a.setAttribute("href", "#");
+    a.setAttribute("class", "navbar__link");
+    navbarList.appendChild(li).appendChild(a);
+
+    //for sections (dynamically)
+    sectionIDs.forEach(createUL)
+}
+
+
+
+// Add class 'active' to the section that is cloest to the top of viewport
+
+function addActiveClass() {
+    //for each section, what's the absolute distance between the section to the top?
+    const distanceToTopArray = []
+    sections.forEach((section) => {
+        const distanceToTop = section.getBoundingClientRect().top;
+        distanceToTopArray.push(Math.abs(distanceToTop));
+    });
+    
+    // get the smallest distance
+    const closest = Math.min(...distanceToTopArray);
+
+    // add the "active" class to the section that is the closest; remove it when it's not
+    sections.forEach((section) => {
+        const distanceToTop = section.getBoundingClientRect().top;
+        if (Math.abs(distanceToTop) === closest){
+            section.classList.add("active");
+        } else {
+            section.classList.remove("active");
+        }
+    });
+
+    // Bonus: make header's opacity 0.7 when scolling down
+    if(window.scrollY > 0){
+        headerTag.setAttribute("style","opacity:0.7; transition: all 1s");
+    } else {
+        headerTag.setAttribute("style","opacity:1; transition: all 1s");
+    }
+
+}
+
+
+// Scroll to anchor ID using scrollTO event
+function scrollToSection(element){
+
+    //prevent default jumping
+    element.preventDefault();
+
+    //get the target element's destination hash
+    const destinationHash = element.target.hash;
+    // console.log(destinationHash)
+    // parse destinationHash to get destinationElementOffsetTop
+    if(destinationHash !== undefined){
+        if(destinationHash === "") {
+            var destinationElementOffsetTop = 0;
+        } else {
+            var destinationElementOffsetTop = document.querySelector(destinationHash).offsetTop;
+        }   
+    }
+
+    // define scrollOptions
+    const scrollOptions = {
+        left: 0,
+        top: destinationElementOffsetTop,
+        behavior: 'smooth'
+    }
+
+    // scroll to that point
+    window.scrollTo(scrollOptions);
+    
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+createHeaderList();
+
+// When the event DOMContentLoaded occurs, it is safe to access the DOM
+document.addEventListener('DOMContentLoaded', function() {
+    //console.log("it's ready!");  
+    // Set sections as active
+    document.addEventListener('scroll', addActiveClass);
+    // When the event DOMContentLoaded occurs, it is safe to access the DOM
+    navbarList.addEventListener('click', scrollToSection);
+});
+
